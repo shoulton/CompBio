5ae2a60b01f66acb28fd44d97959cdaef7a1c223
Sarah <shoulton@rams.colostate.edu>
2022-01-28 18:47:30 -0500

    changed lines stuff

--- Documents/Sarah/Bioinformatics/A1/fasta_translate1.py
+++ Documents/Sarah/Bioinformatics/A1/fasta_translate1.py
@@ -10,13 +10,13 @@ descriptor = ''
 seq = ''
 
 for line in lines:
-
-    if line[0] == '>':
-        if descriptor:
-            seqdict[descriptor] = seq
-            seq = ''
-        descriptor = line[1:]
-    else:
-        seq = seq + line
+    if line:
+        if line[0] == '>':
+            if descriptor:
+                seqdict[descriptor] = seq
+                seq = ''
+            descriptor = line[1:]
+        else:
+            seq = seq + line
 seqdict[descriptor] = seq
 print(seqdict)
