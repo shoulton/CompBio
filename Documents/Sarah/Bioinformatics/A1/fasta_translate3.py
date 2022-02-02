78691a25ee2d1695fdfacd2fe10a6a8d8ff1220f
Sarah <shoulton@rams.colostate.edu>
2022-02-01 20:23:21 -0500

    final commit maybe hmm

--- Documents/Sarah/Bioinformatics/A1/fasta_translate3.py
+++ Documents/Sarah/Bioinformatics/A1/fasta_translate3.py
@@ -44,8 +44,9 @@ def translate(codonDict, fastaDict):
             if len(trio) == 3:
                 acid = codonDict.get(trio, "error")
                 if acid == "error":
-                    sys.stderr( "Found a nucleotide sequence not described in the codon file", file=sys.stderr)
-                translatedSeq = translatedSeq + codonDict.get(trio)
+                    sys.stderr( "Invalid codon: " + trio + "\n")
+                else:
+                    translatedSeq = translatedSeq + codonDict.get(trio)
                 trio = ''
         print(">" + key + "\n" + translatedSeq + "\n")
 
