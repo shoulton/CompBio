9c8f07531dde9fa8246500cc06cc8b116652099a
Sarah <shoulton@rams.colostate.edu>
2022-02-02 16:53:19 -0500

    new test case

--- Documents/Sarah/Bioinformatics/A1/fasta_translate4.py
+++ Documents/Sarah/Bioinformatics/A1/fasta_translate4.py
@@ -41,6 +41,7 @@ def translate(codonDict, fastaDict):
     keys = list(fastaDict.keys())
     for key in keys:
         translatedSeq = ''
+        print(translatedSeq)
         seq = fastaDict[key].strip()
         trio = ''
         for char in seq:
