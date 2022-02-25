3f5f050cf9bb869c808a3a0f85a73d130947f731
Sarah <sarah.houlton15@gmail.com>
2022-02-25 16:51:47 -0700

    so dang close

--- Documents/Sarah/Bioinformatics/A4/mutation_caller.py
+++ Documents/Sarah/Bioinformatics/A4/mutation_caller.py
@@ -7,60 +7,51 @@ def b_given_X_Y(b, x, y):
     p_y = b_given_X(b,y)
 
     ret = 0.5*p_x + 0.5*p_y
-    #print("\t\t" + str(ret))
     return ret
 
 
 def b_given_X(b, x):
     e = 0.1
-    #print("\t\t\t" + b + "  " + x)
     if b == x:
-        score = 1 - e
-       # print("\t\t\t" + str(score))
-        return score
-    score = e/3
-    #print("\t\t\t" + str(score))
+        score = 1.000 - e
+    else:
+        score = e/3.0
     return score
         
 
 def likelihood_of_genotypes(bases):
     diploid_genotypes = [('A','A'), ('C','C'), ('G','G'), ('T','T'), ('A','C'), ('A','G'), ('A','T'), ('C','G'), ('C','T'), ('G','T')]
-    scores = []
-    best_genotype = [('X','Y')]
-    best_score = 0
-    for G in diploid_genotypes:
+    best_genotype = {}
+    #print("bases: ", ",".join(bases))
+    for base in bases:
         score = 1.0
-        for base in bases:
-            score = score * b_given_X_Y(base, G[0], G[1])
-            #print("\t" + str(base)+ "  "  + str(G) + "  "  + str(score))
-        
-        if score > best_score:
-            best_score = score
-            best_genotype = [G]
+        for G in diploid_genotypes:
+            score = b_given_X_Y(base.upper(), G[0], G[1])
+            key = ''.join(G)
+            if key not in best_genotype:
+                best_genotype[key] = 1.0
 
-        if (score == best_score) & (G not in best_genotype):
-            best_genotype.append(G)
-    
-        #genotype_tup = (G[0], G[1], score)
-        #scores.append(genotype_tup)
-    #scores_sorted = scores.sort(key=lambda i:i[2])
-    #print(scores)
-    #print(best_genotype)
-    #print(str(best_score) + "\n" + str(math.log(best_score)))
-    return (best_genotype, math.log(best_score))
+            best_genotype[key] *= score
+        
+    max_key = max(best_genotype, key = best_genotype.get)
+    best_score = math.log(best_genotype[max_key])
+    return (max_key, best_score)
 
 
 def likelihood_of_cancer(bases, G):
-    score = 1.0
-    best_score = 0
+    best_genotype = {}
     for base in bases:
-        for g in G:
-            score = score * b_given_X_Y(base, g[0], g[1])
-            if score < best_score:
-                best_score = score
-        #print("\t" + str(base)+ "  "  + str(G) + "  "  + str(score))
+        score = b_given_X_Y(base.upper(), G[0], G[1])
+        key = ''.join(G)
+        if key not in best_genotype:
+            best_genotype[key] = 1.0
 
-    return (math.log(score))
+        best_genotype[key] *= score
+    print(best_genotype)
+    max_key = max(best_genotype, key = best_genotype.get)
+    best_score = math.log(best_genotype[max_key])
+    print(best_score)
+    return (best_score)
 
 
 def find_somatic(cancer_bam, normal_bam):
@@ -86,19 +77,17 @@ def find_somatic(cancer_bam, normal_bam):
             nbases = []
             for nread in ncol.pileups:
                 nbase = nread.alignment.query_sequence[nread.query_position]
-                #print(str(len(nread.alignment.query_sequence))+ "    " + str(nread.query_position) )
-                nbases.append(nbase)
-            #print("coverage: " + str(n_coverage) +"length of list: " + str(len(nbases)))
+                nbases.append(nbase.upper())
             cbases = []
             for read in col.pileups:
                 base = read.alignment.query_sequence[read.query_position]
-                cbases.append(base)
+                cbases.append(base.upper())
             best_genotype, best_score = likelihood_of_genotypes(nbases)
             if best_score < -50:
-                print("Postiion " + str(pos) + " has ambiguous genotype.")
+                print("Position " + str(pos) + " has ambiguous genotype.")
             else:
+                print(best_genotype)
                 somatic_score = likelihood_of_cancer(cbases, best_genotype)
-                print(somatic_score)
                 if somatic_score < -75:
                     print("Position " + str(pos) + " has a candidate somatic mutation (Log-likelihood= " + str(somatic_score) + ")")
             
