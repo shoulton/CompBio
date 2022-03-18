a91f5808f439cc481f67c32c4284f9a61a44c895
Sarah <shoulton@rams.colostate.edu>
2022-03-17 21:47:41 -0400

    working a5

--- Documents/Sarah/Bioinformatics/A5/fdr.R
+++ Documents/Sarah/Bioinformatics/A5/fdr.R
@@ -5,9 +5,33 @@ spec <- matrix(c(
   'output','o',2, "character"
 ), byrow=TRUE, ncol=4)
 
+#input <- "p05pvalues.tsv"
+#output <- "pvalues.corrected.tsv"
+
 opt = getopt(spec)
 input <- opt$input
 output <- opt$output
 
-data <- read.table(file='p05pvalues.tsv', sep = '\t', header = TRUE, fill = TRUE)
-ordered <- data[order('pvalue'),]
+data <- read.table(file=input, sep = '\t', header = TRUE, fill = TRUE)
+ordered <- data[order(data$pvalue),]
+
+m <- nrow(ordered)
+i <- 1
+
+q_val <- vector("list", m)
+
+for (j in rownames(ordered)) {
+  row <- ordered[i,]
+  ratio <- m/i
+  p_val <- row$pvalue
+  q_val[[i]] <- (p_val*ratio)
+  i <- i+1
+}
+
+ordered$qvalue <- q_val
+
+ordered[sapply(ordered, is.list)] <-
+  sapply(ordered[sapply(ordered, is.list)], 
+         function(x)sapply(x, function(y) paste(unlist(y),collapse=", ") ) )
+
+write.table(ordered, output, quote=FALSE, sep="\t", row.names = FALSE)
