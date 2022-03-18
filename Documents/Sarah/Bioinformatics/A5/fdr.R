a94b6861496c2eb379b87a8dfc95722913fabfd9
Sarah <sarah.houlton15@gmail.com>
2022-03-17 18:52:08 -0600

    opt flags

--- Documents/Sarah/Bioinformatics/A5/fdr.R
+++ Documents/Sarah/Bioinformatics/A5/fdr.R
@@ -1,12 +1,13 @@
 library('getopt')
 
 spec <- matrix(c(
-  'input','i',1,"string",
-  'output','o',1, "string"
+  'input','i',2,"character",
+  'output','o',2, "character"
 ), byrow=TRUE, ncol=4)
 
 opt = getopt(spec)
-print(opt)
+input <- opt$input
+output <- opt$output
 
 data <- read.table(file='p05pvalues.tsv', sep = '\t', header = TRUE, fill = TRUE)
 ordered <- data[order('pvalue'),]
